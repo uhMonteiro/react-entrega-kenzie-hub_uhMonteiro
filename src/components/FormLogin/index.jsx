3dc9bca242ddb61076b02e7ad1e6fb26e88b91d0
Augusto <augustomonteirop@hotmail.com>
2023-06-23 20:52:26 -0300

    criação e login de usuario

--- src/components/FormLogin/index.jsx
+++ src/components/FormLogin/index.jsx
@@ -1,20 +1,45 @@
 import { Link } from "react-router-dom";
 import { FormStyle, H1StyleLogin, InputStyle, LabelStyle } from "./style";
+import { api } from "../../services/api";
+import { useForm } from "react-hook-form";
 
 
 export function FormLogin(){
+  const { register , handleSubmit} = useForm()
+
+  async function LoginUser(formData){
+    try {
+      const {data} = await api.post("/sessions", formData)
+      console.log(data)
+    } catch (error) {
+      console.log("erro")
+    }
+  }
+
+  async function submit(formData){
+    LoginUser(formData)
+  }
+
+  
+
     return(
-        <FormStyle action="">
+        <FormStyle onSubmit={handleSubmit(submit)}>
             <H1StyleLogin>Login</H1StyleLogin>
             <LabelStyle>Email
-              <InputStyle type="email" placeholder="Digite seu email"/>
+              <InputStyle 
+              type="email" 
+              placeholder="Digite seu email"
+              required
+              {...register("email")}/>
             </LabelStyle>
             <LabelStyle>Senha
-              <InputStyle type="password" placeholder="Digite sua senha"/>
+              <InputStyle 
+              type="password" 
+              placeholder="Digite sua senha"
+              required
+              {...register("password")}/>
             </LabelStyle>
-            <Link to="/homePage">
               <button>Entrar</button>
-            </Link>
             <Link to="/registerPage"> 
               <button>Cadastre-se</button>
             </Link>
--- src/components/FormRegister/index.jsx
+++ src/components/FormRegister/index.jsx
@@ -1,34 +1,85 @@
+import { useForm } from "react-hook-form";
 import { FormStyle, LabelStyle } from "../FormLogin/style";
+import { api } from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 
 export function FormRegister(){
+    const { register , handleSubmit , reset} = useForm()
+
+    const navigate = useNavigate()
+
+    async function createUser(formData){
+      try {
+        const {data} = await api.post("/users", formData)
+        navigate("/")
+        console.log(data)
+      } catch (error) {
+        console.log("erro")
+      }
+    }
+
+    async function submit(formData){
+      createUser(formData)
+      reset()
+    }
+
     return(
-        <FormStyle action="">
+        <FormStyle onSubmit={handleSubmit(submit)}>
             <h1>Crie sua conta</h1>
             <p>Rapido e grátis, vamos nessa</p>
             <LabelStyle>Nome
-              <input type="text" placeholder="Digite aqui seu nome"/>
+              <input 
+              type="text" 
+              placeholder="Digite aqui seu nome"
+              required
+              {...register("name")}/>
             </LabelStyle>
             <LabelStyle>Email
-              <input type="email" placeholder="Digite aqui seu email"/>
+              <input 
+              type="email" 
+              placeholder="Digite aqui seu email"
+              required
+              {...register("email")}/>
             </LabelStyle>
             <LabelStyle>Senha
-              <input type="password" placeholder="Digite aqui sua senha"/>
+              <input 
+              type="password" 
+              placeholder="Digite aqui sua senha"
+              required
+              {...register("password")}/>
             </LabelStyle>
             <LabelStyle>Confirmar Senha
-              <input type="password" placeholder="Digite novamente sua senha"/>
+              <input 
+              type="password" 
+              placeholder="Digite novamente sua senha"
+              required
+              {...register("confirmedPassword")}/>
             </LabelStyle>
             <LabelStyle>Bio
-              <input type="text" placeholder="Fale sobre você"/>
+              <input 
+              type="text" 
+              placeholder="Fale sobre você"
+              required
+              {...register("bio")}/>
             </LabelStyle>
             <LabelStyle>Contato
-              <input type="text" placeholder="Opção de contato"/>
+              <input 
+              type="text" 
+              placeholder="Opção de contato"
+              required
+              {...register("contact")}/>
             </LabelStyle>
             <LabelStyle>Selecionar módulo
-              <select name="" id="">
-                  <option value="">Primeiro Módulo</option>
+              <select name="" id="" {...register("course_module")} required>
+                  <option value="">Selecionar Modulo</option>
+                  <option value="Primeiro módulo">Primeiro módulo</option>
+                  <option value="Segundo módulo">Segundo módulo</option>
+                  <option value="Terceiro módulo">Terceiro módulo</option>
+                  <option value="Quarto módulo">Quarto módulo</option>
               </select>
             </LabelStyle>
+            <button>Cadastrar</button>
         </FormStyle>
     )
 }
